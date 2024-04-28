091adf4f0e88aa3fd5ccbcb622db1a7d27c83f97
Maksym Riznyk <reznikmaksym14@gmail.com>
2024-04-28 23:33:01 +0200

    add side drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 import Image from "next/image";
 import Header from "@/app/components/Header";
+import SideDrawer from "@/app/components/Drawer";
+
 import { useState } from "react";
 
 export default function Home() {
   const [sliderOpened, setSliderOpened] = useState(false);
 
-  return <Header setSliderOpened={setSliderOpened}></Header>;
+  return (
+    <div>
+      <Header setSliderOpened={setSliderOpened}></Header>
+      <SideDrawer
+        sliderOpened={sliderOpened}
+        setSliderOpened={setSliderOpened}
+      ></SideDrawer>
+    </div>
+  );
 }
