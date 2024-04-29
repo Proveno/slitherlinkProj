0a16005ee83a1d195948220a9b33ac5b3bfb23a4
Maksym Riznyk <reznikmaksym14@gmail.com>
2024-04-29 14:05:28 +0200

    fix popupfooter, structure main view and comments place

--- src/app/components/PopUpFooter.tsx
+++ src/app/components/PopUpFooter.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 const PopUpFooter = (props: any) => {
     return (
         <footer
-            className="bg-[#28224F] p-2 sticky z-[20] mx-auto fixed bottom-0 w-full">
+            className="bg-[#28224F] p-2 absolute z-[20] mx-auto bottom-[63px] w-full">
             <div className="flex flex-wrap items-center justify-center">
                 <p style={{color: '#bfc2cf', paddingRight: '10px'}}>Don't lose your progress!</p>
                 <Button variant="outlined" color="primary">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,29 @@ import Footer from "@/app/components/Footer";
 
 import SideDrawer from "@/app/components/Drawer";
 
-import {useState} from "react";
+import { useState } from "react";
 import PopUpFooter from "@/app/components/PopUpFooter";
 
+import MainPart from "@/app/components/MainPart";
+
 export default function Home() {
-    const [sliderOpened, setSliderOpened] = useState(false);
+  const [sliderOpened, setSliderOpened] = useState(false);
 
-    return (
-        <div>
-
-            <Header setSliderOpened={setSliderOpened}></Header>
-            <PopUpFooter></PopUpFooter>
-            <Footer></Footer>
-            <SideDrawer
-                sliderOpened={sliderOpened}
-                setSliderOpened={setSliderOpened}
-            ></SideDrawer>
+  return (
+    <div className="flex">
+      <Header setSliderOpened={setSliderOpened}></Header>
+      <SideDrawer
+        sliderOpened={sliderOpened}
+        setSliderOpened={setSliderOpened}
+      ></SideDrawer>
+      <div className="w-full h-screen relative top-0 pt-[72px] bg-zinc-900">
+        <div className="bg-zinc-100 text-center pt-48 h-full text-black relative pb-[114px]">
+          Contrast view
         </div>
-    );
+        <PopUpFooter></PopUpFooter>
+        <Footer></Footer>
+      </div>
+      <div className="pt-[72px] w-1/5 min-w-[14rem]">Comments</div>
+    </div>
+  );
 }
